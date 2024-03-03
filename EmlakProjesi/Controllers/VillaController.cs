b8f26cc8d4dadff6d7c066f59dccd59437322915
muratapk <muratapk@hotmail.com>
2024-03-03 12:46:22 +0300

    Add project files.

---
+++ EmlakProjesi/Controllers/VillaController.cs
@@ -0,0 +1,20 @@
+﻿using DataAccessLayer.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmlakProjesi.Controllers
+{
+    public class VillaController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public VillaController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ EntityLayer/Entire/Villa.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Entire
+{
+    public class Villa
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public double Price { get; set; }
+        public string ImageUrl { get; set; } = string.Empty;
+        public int? Occupancy { get; set; }
+        //doluluk durumu
+        public DateTime? CreateDate { get; set; }
+        public DateTime? UpdateDate { get; set;}
+    }
+}
