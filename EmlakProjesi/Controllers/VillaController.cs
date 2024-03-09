710cdab2fcf5e95ef41fa247e9a6d98e28fe193b
muratapk <muratapk@hotmail.com>
2024-03-09 10:27:48 +0300

    emlak sitesi

--- EmlakProjesi/Controllers/VillaController.cs
+++ EmlakProjesi/Controllers/VillaController.cs
@@ -1,4 +1,5 @@
 ﻿using DataAccessLayer.Data;
+using EntityLayer.Entire;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EmlakProjesi.Controllers
@@ -16,6 +17,36 @@ namespace EmlakProjesi.Controllers
         {
             var sorgu = _db.Villas.ToList();
             return View(sorgu);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Villa entity )
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Edit(Villa entity)
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Delete(Villa entity)
+        {
+            return View();
         }
     }
 }
