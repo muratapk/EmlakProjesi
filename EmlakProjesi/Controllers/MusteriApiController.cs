22b5dd3997aca74b4646776a48106d6674ec525c
muratapk <muratapk@hotmail.com>
2024-04-06 11:37:27 +0300

    web api eklenmesi

--- EmlakProjesi/Controllers/MusteriApiController.cs
+++ EmlakProjesi/Controllers/MusteriApiController.cs
@@ -1,14 +1,34 @@
 ﻿using EntityLayer.Entire;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace EmlakProjesi.Controllers
 {
     public class MusteriApiController : Controller
     {
         Uri baseAddress = new Uri("https://localhost:7019/api/");
+        private readonly HttpClient _client;
+
+        public MusteriApiController()
+        {
+            _client = new HttpClient();
+            _client.BaseAddress = baseAddress;
+        }
+        [HttpGet]
         public IActionResult Index()
         {
-            return View();
+            List<Musteri> musteriler = new List<Musteri>();
+            //size boş bir musteri listesi nesnesi 
+            HttpResponseMessage response = _client.GetAsync(_client.BaseAddress+"/Musteri/").Result;
+
+            if(response.IsSuccessStatusCode)
+            {
+                string data= response.Content.ReadAsStringAsync().Result;
+                //gelen datayı api ile gelen içeriği al
+                musteriler=JsonConvert.DeserializeObject<List<Musteri>>(data);
+                //aldığım içeriği JsonConvert.Deserialize oku Musteri Listesi oku
+            }
+            return View(musteriler);
         }
         public IActionResult Create()
         {
