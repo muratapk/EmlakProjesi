0ce91b7d517f88c4393cb0d5df4476f48ff0c339
muratapk <muratapk@hotmail.com>
2024-04-06 10:57:03 +0300

    Api Üzerinden veri kayıt etme ve güncelleme

---
+++ EmlakProjesi/Areas/Home/EfeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EmlakProjesi.Areas.Home
+{
+    public class EfeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ EmlakProjesi/Controllers/MusteriApiController.cs
@@ -0,0 +1,47 @@
+﻿using EntityLayer.Entire;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmlakProjesi.Controllers
+{
+    public class MusteriApiController : Controller
+    {
+        Uri baseAddress = new Uri("https://localhost:7019/api");
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Musteri musteri)
+        {
+            return View();
+        }
+        public IActionResult Delete(int id)
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Delete(Musteri musteri,int id)
+        {
+
+            return View();
+        }
+        public IActionResult Update(int id)
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Update(Musteri musteri, int id)
+        {
+
+            return View();
+        }
+
+
+    }
+}
